6b0cae0e1afc6fa057bd78bc29fcf0644164ec1c
Ranjith BR <ranjith19@gmail.com>
2026-08-27 11:14:08 +0530

    Make tmux tabs clickable

--- scripts/status.sh
+++ scripts/status.sh
@@ -45,11 +45,11 @@ print_tabs() {
         color_for_window "$window_id"
         color="$WINDOW_COLOR"
         if [[ "$active" == "1" ]]; then
-            printf '#[range=window|%s,fg=colour%s,bg=colour%s,bold] ● %s  %s #[fg=colour%s,bg=colour%s]#[range=none,nobold]' \
-                "$window_id" "$BACKGROUND" "$color" "$index" "$name" "$color" "$BACKGROUND"
+            printf '#[range=window|%s,fg=colour%s,bg=colour%s,bold] ● %s  %s #[fg=colour%s,bg=colour%s]#[norange,nobold]' \
+                "$index" "$BACKGROUND" "$color" "$index" "$name" "$color" "$BACKGROUND"
         else
-            printf '#[range=window|%s,fg=colour255,bg=colour%s] %s  %s #[fg=colour%s,bg=colour%s]#[range=none]' \
-                "$window_id" "$color" "$index" "$name" "$color" "$BACKGROUND"
+            printf '#[range=window|%s,fg=colour255,bg=colour%s] %s  %s #[fg=colour%s,bg=colour%s]#[norange]' \
+                "$index" "$color" "$index" "$name" "$color" "$BACKGROUND"
         fi
     done < <(tmux list-windows -F $'#{window_id}\t#{window_index}\t#{window_name}\t#{window_active}')
 }
